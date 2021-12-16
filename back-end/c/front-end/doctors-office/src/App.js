54c4bba30016d928552bf18a21b39dae2ba8f8a7
Lieba Pil <pil.lieba@gmail.com>
2021-12-16 11:18:52 -0800

    nav bar done

--- back-end/c/front-end/doctors-office/src/App.js
+++ back-end/c/front-end/doctors-office/src/App.js
@@ -4,17 +4,20 @@ import Contact from './routes/Contact';
 import Doctor from './routes/Doctor';
 import Patient from './routes/Patient';
 import Home from './routes/home';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/doctor" component={Doctor}/>
-        <Route path="/patient" component={Patient}/>
-        <Route path="/contact" component={Contact}/>
-      </Switch>
-      
+      <Nav/>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/doctor" component={Doctor}/>
+            <Route path="/patient" component={Patient}/>
+            <Route path="/contact" component={Contact}/>
+          </Switch>
+        </main>
     </div>
   );
 }
