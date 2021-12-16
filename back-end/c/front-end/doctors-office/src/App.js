cc072819501dbfa93a4ed2540761f281dff60593
Lieba Pil <pil.lieba@gmail.com>
2021-12-15 17:21:47 -0800

    added the raoutes in frontend

--- back-end/c/front-end/doctors-office/src/App.js
+++ back-end/c/front-end/doctors-office/src/App.js
@@ -1,10 +1,19 @@
-
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import Contact from './routes/Contact';
+import Doctor from './routes/Doctor';
+import Patient from './routes/Patient';
+import Home from './routes/home';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/doctor" component={Doctor}/>
+        <Route path="/patient" component={Patient}/>
+        <Route path="/contact" component={Contact}/>
+      </Switch>
       
     </div>
   );
