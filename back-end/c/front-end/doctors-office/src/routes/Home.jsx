dc9b3f36a962055d68facc4703b03b9dc84d5f58
Lieba Pil <pil.lieba@gmail.com>
2021-12-17 11:24:38 -0800

    home page complete

--- back-end/c/front-end/doctors-office/src/routes/Home.jsx
+++ back-end/c/front-end/doctors-office/src/routes/Home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
         <div>
             <img src="https://i.pinimg.com/originals/3f/dd/8d/3fdd8d0ae46fd4520cae854ff837afac.png" alt="doctor office front" />
             <h1>MDUDO Internal Medicine</h1>
-            <p></p>
+            <p>Our practice specializes in Internal medicine. We welcome everyone from all types of backgrounds. We have an OBGYN on-premises to help take care of all your needs. Our Doctors, Physcian assistants, Nurse practitioners, and Medical Assistants are all professional and welcoming. Each brings in their professional practice and knowledge to incorporate into the work we do here.  
+</p>
 
             
         </div>
