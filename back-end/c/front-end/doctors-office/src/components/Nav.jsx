0c8890775dca01e3ee76b5c94b5e853d839dcb33
Lieba Pil <pil.lieba@gmail.com>
2021-12-17 11:45:14 -0800

    nav bar complete

--- back-end/c/front-end/doctors-office/src/components/Nav.jsx
+++ back-end/c/front-end/doctors-office/src/components/Nav.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return (
-        <div>
-            
+        <div className='bar'>
+            <Link to='/'>Home</Link>
+            <Link to='/doctor'>Doctor Page</Link>
+            <Link to='/patient'>Sign up</Link>
+            <Link to='/contact'>Contact</Link>   
         </div>
     )
 }
