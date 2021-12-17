7453909779295466ebd6f99f33ae83217c624054
Lieba Pil <pil.lieba@gmail.com>
2021-12-17 06:48:00 -0800

    finished the useState for doctors

--- back-end/c/front-end/doctors-office/src/components/Doctors.jsx
+++ back-end/c/front-end/doctors-office/src/components/Doctors.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-export default function Doctors() {
+export default function Doctors(props) {
     return (
-        <div>
+        <div className='doctor'>
+        <img src={imgUrl} alt={props.name}/>
+        <div className='description'>
+            <h4>Doctor: {props.name}</h4>
+            <h5>Specialty: {props.specialty}</h5>
+            <p className='explantion'>About the Doctor: {props.description}</p>
+            <p className='years'>Years of Practice: {yearsOfPractice}</p>
+        </div>
             
         </div>
     )
--- back-end/c/front-end/doctors-office/src/routes/Doctor.jsx
+++ back-end/c/front-end/doctors-office/src/routes/Doctor.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Doctors from '../components/Doctors'
+import axios from 'axios'
 
+//// most of this looks similar to my group project since it's pretty much the same layout
 export default function Doctor() {
+
+    const [doctors, setDoctors] = useState([])
+
+    useEffect(()=>{
+        const getDoctors = async () =>{
+            let doctor = await axios.get('http://localhost:3001/doctor')
+            setDoctors(doctor.data)
+        }
+        getDoctors()
+    },[])
+
     return (
-        <div>
+        <div className='container'>
             
         </div>
     )
