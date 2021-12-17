8604955a7bf83d16839e37c1e4301c26337d23e2
Lieba Pil <pil.lieba@gmail.com>
2021-12-17 09:18:22 -0800

    almost done homwpage

--- back-end/c/front-end/doctors-office/src/routes/Home.jsx
+++ back-end/c/front-end/doctors-office/src/routes/Home.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
+            <img src="https://i.pinimg.com/originals/3f/dd/8d/3fdd8d0ae46fd4520cae854ff837afac.png" alt="doctor office front" />
+            <h1>MDUDO Internal Medicine</h1>
+            <p></p>
+
             
         </div>
     )
