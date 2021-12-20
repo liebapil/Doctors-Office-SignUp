517331c88161f5b3b03003f2f7781651edb29bca
Lieba Pil <pil.lieba@gmail.com>
2021-12-19 18:10:22 -0800

    form done but not working

--- back-end/c/front-end/doctors-office/src/components/Form.jsx
+++ back-end/c/front-end/doctors-office/src/components/Form.jsx
@@ -2,12 +2,67 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
-
+//// the same with this page looks similar since a lot of it is the same sytax 
 export default function Form() {
+
+    const [firstName, setFirstName]= useState('')
+    const [lastName, setLastName]= useState('')
+    const [DOB, setDOB]= useState('')
+    const [gender, setGender]= useState('')
+    const [socialSecurity, setSocialSecurity]= useState('')
+    const [submit, setSubmit] = useState(false);
+
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+    }
+
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+    }
+
+    const handleDOB = (e) => {
+        setDOB(e.target.value);
+    }
+
+    const handleGender = (e) => {
+        setGender(e.target.value);
+    }
+
+    const handleSocialSecurity = (e) => {
+        setSocialSecurity(e.target.value);
+    }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (submit === true){
+            return
+        }
+        setSubmit(true)
+        await axios.post("http://localhost:3001/patient", {
+            firstName: firstName,
+            lastName: lastName,
+            dateOfBirth: DOB,
+            gender: gender,
+            socialSecurity: socialSecurity
+        })}
+
+
+
     return (
         <div>
-           
-            
+            <form onSubmit={handleSubmit}>
+                <input onChange={handleFirstName} type='text' placeholder='First Name' value={firstName}>First Name:</input>
+                <input onChange={handleLastName} type='text' placeholder='Last Name' value={lastName}>Last Name:</input>
+                <input onChange={handleDOB} type='date' placeholder='Date Of Birth' value={DOB}>Date Of Birth:</input>
+                <select onChange={handleGender} type='text' placeholder='Gender' value={gender}> Gender:
+                    <option>Male</option>
+                    <option>Female</option>
+                    <option>Unknown</option>
+                </select>
+                <input onChange={handleSocialSecurity} type='number' placeholder='Social Security'>Social Security:</input>
+                <button type='submit'>Submit</button>
+            </form>   
         </div>
     )
 }
