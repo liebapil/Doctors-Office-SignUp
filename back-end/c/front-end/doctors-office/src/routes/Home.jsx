6003e499504355885403084cc065afa532a49385
Lieba Pil <pil.lieba@gmail.com>
2021-12-20 08:16:37 -0800

    finished most of doctors page css

--- back-end/c/front-end/doctors-office/src/routes/Home.jsx
+++ back-end/c/front-end/doctors-office/src/routes/Home.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Home() {
     return (
         <div>
-            <img src="https://i.pinimg.com/originals/3f/dd/8d/3fdd8d0ae46fd4520cae854ff837afac.png" alt="doctor office front" />
+            <img className='home-img' src="https://i.pinimg.com/originals/3f/dd/8d/3fdd8d0ae46fd4520cae854ff837afac.png" alt="doctor office front" />
             <h1>MDUDO Internal Medicine</h1>
-            <p>Our practice specializes in Internal medicine. We welcome everyone from all types of backgrounds. We have an OBGYN on-premises to help take care of all your needs. Our Doctors, Physcian assistants, Nurse practitioners, and Medical Assistants are all professional and welcoming. Each brings in their professional practice and knowledge to incorporate into the work we do here.  
+            <p className='home-page-text'>Our practice specializes in Internal medicine. We welcome everyone from all types of backgrounds. We have an OBGYN on-premises to help take care of all your needs. Our Doctors, Physcian assistants, Nurse practitioners, and Medical Assistants are all professional and welcoming. Each brings in their professional practice and knowledge to incorporate into the work we do here.  
 </p>
 
             
