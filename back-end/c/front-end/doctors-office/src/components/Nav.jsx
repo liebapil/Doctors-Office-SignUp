2c9429e033ca94651ac2c7610c2c4e607540070b
Lieba Pil <pil.lieba@gmail.com>
2021-12-20 08:04:20 -0800

    first attemot at icon

--- back-end/c/front-end/doctors-office/src/components/Nav.jsx
+++ back-end/c/front-end/doctors-office/src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export default function Nav() {
     return (
         <div className='bar'>
+            <img src="./img/hospital.png" alt="hospital" />
             <Link to='/' className='link'>Home</Link>
             <Link to='/doctor' className='link'>Doctor Page</Link>
             <Link to='/patient' className='link'>Sign up</Link>
