2792690459383c00fa17af37fece8d3db292361a
Lieba Pil <pil.lieba@gmail.com>
2021-12-23 08:26:25 -0800

    retry

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const PORT = process.env.PORT || 3001;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(`${__dirname}/client/build/index.html`))
-    })
-  }
 
 
 
@@ -23,9 +17,15 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
- app.use('/doctor', routing.doctor)
- app.use('/patient', routing.patient)
+app.use('/doctor', routing.doctor)
+app.use('/patient', routing.patient)
 
 
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    })
+  }
 app.listen(PORT, () => console.log(`Server Listening on port: ${PORT}`));
